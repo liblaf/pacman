e310ce965da065cb5c2fa62e34d50969df0f8a2a
Qin Li <liblaf@outlook.com>
2022-03-29 21:19:30 +0800

    test problem 1

---
+++ test.py
@@ -0,0 +1,58 @@
+import re
+import os
+import sys
+
+
+def get_layouts(path: str = "./layouts/") -> list:
+    layouts = []
+    for filename in os.listdir(path=path):
+        match = re.match(pattern="(\w*).lay", string=filename)
+        if match:
+            layouts.append(match.group(1))
+    return layouts
+
+
+def prepare_problem_1():
+    layouts = ["bigMaze", "mediumMaze", "testMaze", "tinyMaze"]
+    fns = ["depthFirstSearch", "breadthFirstSearch", "uniformCostSearch", "aStarSearch"]
+    heuristics = ["nullHeuristic", "euclideanHeuristic", "yourHeuristic"]
+    argvs = []
+    for layout in layouts:
+        for fn in fns:
+            if fn == "aStarSearch":
+                for heuristic in heuristics:
+                    argvs.append(
+                        {
+                            "layout": layout,
+                            "pacman": "SearchAgent",
+                            "agentArgs": {"fn": fn, "heuristic": heuristic},
+                        }
+                    )
+            else:
+                argvs.append(
+                    {"layout": layout, "pacman": "SearchAgent", "agentArgs": {"fn": fn}}
+                )
+    return argvs
+
+
+if __name__ == "__main__":
+    argvs = []
+    if (len(sys.argv) < 2) or (sys.argv[1] == "1"):
+        argvs = prepare_problem_1()
+    else:
+        pass
+    for argv in argvs:
+        cmd = [
+            "python",
+            "pacman.py",
+            "--quietTextGraphics",
+            f"--layout={argv['layout']}",
+            f"--pacman={argv['pacman']}",
+        ]
+        if "agentArgs" in argv:
+            agentArgs = []
+            for key, value in argv["agentArgs"].items():
+                agentArgs.append(f"{key}={value}")
+            agentArgs = ",".join(agentArgs)
+            cmd.append(f"--agentArgs={agentArgs}")
+        os.system(" ".join(cmd))
